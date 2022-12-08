7ca82a5bab6c0e8737833d2ab58ef850aaa21631
Marcela <mayorohe.0606@gmail.com>
2022-12-08 14:53:46 -0600

    vistas

--- app/src/main/java/mx/basher/battleship/WinnerActivity.kt
+++ app/src/main/java/mx/basher/battleship/WinnerActivity.kt
@@ -1,10 +1,14 @@
 package mx.basher.battleship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class WinnerActivity : AppCompatActivity() {
+    lateinit var volverjugar_btn: Button
+    lateinit var fin_btn: Button
     private lateinit var winnerTXT: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +17,24 @@ class WinnerActivity : AppCompatActivity() {
 
         winnerTXT = findViewById(R.id.winnerTXT)
 
+        volverjugar_btn = findViewById(R.id.volverjugar_btn)
+        fin_btn = findViewById(R.id.fin_btn)
+
         //recuperar el nombre del jugador
         var nombreJugador= intent.getStringExtra("Nombre")
 
 
         winnerTXT.text = nombreJugador.toString()
 
+        volverjugar_btn.setOnClickListener(){
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
 
+        fin_btn.setOnClickListener(){
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
 
     }
 }
